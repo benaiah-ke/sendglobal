6f3f49d49dde6d697140bd834245822063b50b72
benaiah-ke <benaiahpraise@gmail.com>
2022-09-02 11:24:24 +0300

    Fetch logged in user transactions, send money and add log out function

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ var loggedInUser = null;
 // UI Screens
 const loginScreen = document.querySelector('.login');
 const mainScreen = document.querySelector('.main');
+const sendMoneyScreen = document.querySelector('.send_money');
 
 // When app starts, we enable a user to log in
 login();
+
+// Listeners
+document.querySelector('#logout_btn').addEventListener('click', logout);
+document.querySelector('#send_money_btn').addEventListener('click', sendMoney);
 
 function login() {
     // Hide the main screen and show the login screen
@@ -71,5 +76,101 @@ function updateMainScreen() {
 
 // Get the logged in user's transactions
 function getLatestTransactions(){
-    
+    fetch("http://localhost:3000/transactions?user_id=" + loggedInUser.id)
+            .then(response => response.json())
+            .then(transactions => {
+                loggedInUser.transactions = transactions;
+
+                // Show the transactions and balance
+                updateTransactionUi();
+            });
+}
+
+function updateTransactionUi(){
+    var balance = 0;
+
+    const transactionList = document.querySelector(".transactions");
+    loggedInUser.transactions.forEach((transaction) => {
+        // Update balance
+        balance += transaction.creditAmount;
+        balance -= transaction.debitAmount;
+
+        // Add to list of transactions
+        transactionList.innerHTML = '';
+        transactionList.innerHTML += `
+            <li>
+            ${transaction.type}-
+            ${transaction.creditAmount}-
+            ${transaction.debitAmount}-
+            </li>
+        `;
+    });
+
+    document.querySelector('#balance').innerText = loggedInUser.currency + ' ' + balance;
+}
+
+
+function sendMoney(){
+    // Hide the main screen and show send money screen
+    mainScreen.classList.add('hidden');
+    sendMoneyScreen.classList.remove('hidden');
+
+    document.querySelector('.send_money_form').addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        // Input fields
+        var emailField = document.querySelector('.send_money_form .email');
+        var amountField = document.querySelector('.send_money_form .amount');
+
+        // Ensure user has enough balance
+        var amount = amountField.value;
+        if(amount > loggedInUser.balance){
+            alert("Amount exceeds your balance!");
+            return;
+        }
+
+        // Check if the recepient exists
+        fetch("http://localhost:3000/users?email=" + emailField.value)
+            .then(response => response.json())
+            .then(users => {
+                // We only expect a single user if the user exists
+                // 0 if invalid credentials
+                if(users.length == 0){
+                    alert("The recepient does not exist");
+                }else{
+                    var receivingUser = users[0];
+
+                    sendToUser(receivingUser, amount);
+
+                    // Clear fields
+                    emailField.value = '';
+                    amountField.value = '';
+                }
+            });
+    })
+}
+
+function sendToUser(receivingUser, amount){
+    var time = "2022-09-01 10:43"
+
+    // Create the two transactions
+    var send_transaction = {
+        user_id: loggedInUser.id,
+        type: "Money Transfer",
+        debitAmount: amount,
+        creditAmount: 0,
+        time: time
+    };
+
+    var receive_transaction = {
+        user_id: receivingUser.id,
+        type: "Money Received",
+        debitAmount: 0,
+        creditAmount: amount,
+        time: time
+    };
+}
+
+function getConvertedAmount(amount, currencyFrom, currencyTo) {
+
 }
